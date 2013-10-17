1169b6ae7211d1f40d18dc33436eaae0a228328e
boardstretcher <szornes@outlook.com>
2013-10-17 16:59:06 +0000

    added an about page to say thanks to the projects i am utilizing

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php include "header.php"; ?>
+
+	<legend>projects et al that Orchestra or its Developer utilizes</legend>
+	<p>
+		
+		<a href="https://github.com/">Github</a><br>
+		<a href="https://github.com/php/php-src">The PHP Interpreter</a><br>
+		<a href="http://getbootstrap.com/2.3.2/">Twitter Bootstrap</a><br>
+		<a href="https://github.com/Codiad/Codiad">Codiad Editor</a><br>
+		<a href="http://wiki.centos.org/FrontPage">Centos el6</a><br>
+		<a href="https://www.digitalocean.com/">Digital Ocean VM</a><br>
+	</p>
+
+<?php include "footer.php"; ?>	
--- navbar.php
+++ navbar.php
@@ -6,7 +6,7 @@
 <span class="icon-bar"></span>
 <span class="icon-bar"></span>
 </button>
-<a class="brand" href="./index.php">Orchestra</a>
+<a class="brand" href="./about.php">Orchestra</a>
 <div class="nav-collapse collapse">
 <ul class="nav">
 
