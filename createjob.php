cc81f16be0a4949bb561a45e05464823a9ba4c72
boardstretcher <szornes@outlook.com>
2013-10-19 17:53:39 +0000

    initial creation of createjob page
    update server list modal to include table with buttons
    
    parse server list from /servers/*.ini info

---
+++ createjob.php
@@ -0,0 +1,41 @@
+<?php include 'header.php' ?>
+<?php include 'navbar.php' ?>
+
+<form action="engine.php" method="post">
+<input type="hidden" name="informer" value="key">
+<input type="hidden" name="formname" value="send">
+<h4>job name</h4>
+<input class='span3' type="text" name="jobname"><br>
+<h4>ssh key</h4>
+<select id="key" name="key">
+<?php
+	$dir = './keys/';
+	$files = scandir($dir);
+	foreach($files as $ind_file){
+	if ($ind_file == '.') { continue; }
+	elseif ($ind_file == '..') { continue; }
+	else { 
+	echo "<option value=\"$ind_file\">$ind_file</option><br>";
+	}
+	};
+?>
+</select><br>
+<p>
+<h4>default servers</h4>
+<?php           
+        $dir = './servers/';
+        $files = scandir($dir);
+        foreach($files as $ind_file){
+        if ($ind_file == '.') { continue; }
+        elseif ($ind_file == '..') { continue; }
+        else {                  
+        echo "<input type=\"checkbox\" name=\"servers[]\" value=\"$ind_file\">$ind_file</option><br>";
+        }                       
+        };              
+?>
+            
+</p>
+<textarea class="span3" name="variables" rows=10 placeholder="required variables on seperate lines"></textarea><br>
+<textarea class="span12" name="script" rows=20 placeholder="paste/write your script here..."></textarea><br>
+<button class="btn btn-primary" type="submit">Save Job</button>
+</form>
--- modals/listserversModal.php
+++ modals/listserversModal.php
@@ -11,27 +11,40 @@
     <h3 id="listserversModalLabel">Server list</h3>
   </div>
   <div class="modal-body">
-    <p>
-                        <ul>
-                        <?php
-                        $dir = './servers/';
-                        $files = scandir($dir);
-                        foreach($files as $ind_file){
-                                if ($ind_file == '.') {
-                                        continue; }
-                                        elseif ($ind_file == '..') {
-                                        continue; }
-                                        else {?>
-                                        <li><a href="<?php echo $dir."/".$ind_file;?>"><?php echo $ind_file;?></a></li>
-                        <?php
-                        }};
-                        ?>
-                        </ul>
-    </p>
+
+<table class='table'>
+<thead>
+<tr>
+<th>Hostname</th>
+<th>IP</th>
+<th>Connect?</th>
+</tr>
+<tbody>
+	<?php
+        $dir = './servers/';
+        $files = scandir($dir);
+        foreach($files as $ind_file){
+                if ($ind_file == '.') { continue; }
+                elseif ($ind_file == '..') { continue; }
+                else {
+                        $ind_file="$dir$ind_file";
+                        $settings=parse_ini_file($ind_file);
+                        $hostname=$settings["hostname"];
+                        $ip_address=$settings["ip_address"];
+                        $fp = @fsockopen ($hostname, 22, $errno, $errstr, $timeout);
+                        if ($fp) {
+                                echo "<tr><td>$hostname</td><td>$ip_address</td><td><button class='btn btn-success'>Success</button></td></tr>";
+                                }else{
+                                echo "<tr><td>$hostname</td><td>$ip_address</td><td><button class='btn btn-danger'>Failed</button></td></tr>";
+                        }
+                }
+        };
+	?>
+</tbody>
+</table>
   </div>
   <div class="modal-footer">
     <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-    <button class="btn btn-primary" type="submit">Send Key</button>
   </div>
 </form>
 </div>
